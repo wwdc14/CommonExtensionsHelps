93c1cf30da727279280b2d7fdcc3350f1996a043
hy <wwdc14@vip.qq.com>
2020-06-09 21:45:48 +0800

    update packages

--- Package.swift
+++ Package.swift
@@ -5,36 +5,33 @@ import PackageDescription
 
 let package = Package(
     name: "CommonExtensionsHelps",
-    platforms: [
-        .iOS(.v11)
-    ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "CommonExtensionsHelps", targets: ["CommonExtensionsHelps"]),
+            name: "CommonExtensionsHelps",
+            targets: ["CommonExtensionsHelps"]),
     ],
     dependencies: [
+        
         .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "14.0.0")),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.1.1")),
-        //        .package(url: "https://gitee.com/WWDC14/Moya.git", .upToNextMajor(from: "14.0.0")),
-        .package(url: "https://gitee.com/WWDC14/SnapKit.git", .upToNextMajor(from: "5.0.1")),
-        .package(url: "https://gitee.com/WWDC14/MBProgressHUD.git", .upToNextMajor(from: "1.2.0")),
-        //        .package(url: "https://gitee.com/WWDC14/RxSwift.git", .upToNextMajor(from: "5.1.1")),
-        .package(url: "https://gitee.com/WWDC14/Kingfisher.git", .upToNextMajor(from: "5.14.0")),
-        .package(url: "https://gitee.com/WWDC14/SwiftyJSON.git", .upToNextMajor(from: "5.0.0")),
+        .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1")),
+        .package(url: "https://github.com/jdg/MBProgressHUD.git", .upToNextMajor(from: "1.2.0")),
+        .package(url: "https://github.com/onevcat/Kingfisher.git", .upToNextMajor(from: "5.14.0")),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", .upToNextMajor(from: "5.0.0")),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.1.1"))
     ],
     targets: [
         .target(name: "CommonExtensionsHelps",
-                dependencies: [
-                    "Moya",
-                    "RxMoya",
-                    "RxSwift",
-                    "RxCocoa",
-                    "MBProgressHUD",
-                    "Kingfisher",
-                    "SwiftyJSON",
-                    "SnapKit"
-            ],
-                path: "CommonExtensionsHelps/Source")
-    ]
+                dependencies: [Target.Dependency.byName(name: "SwiftyJSON"),
+                                                              Target.Dependency.byName(name: "MBProgressHUD"),
+                                                              Target.Dependency.byName(name: "Kingfisher"),
+                                                              Target.Dependency.byName(name: "RxSwift"),
+                                                              Target.Dependency.byName(name: "SnapKit"),
+                                                              Target.Dependency.product(name: "RxMoya", package: "Moya"),
+                                                              Target.Dependency.product(name: "RxCocoa", package: "RxSwift")],
+                path: "CommonExtensionsHelps/Source"),
+        .testTarget(
+            name: "CommonExtensionsHelpsTests",
+            dependencies: ["CommonExtensionsHelps"]),
+    ],
+    swiftLanguageVersions: [.v5]
 )
