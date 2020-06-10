5cff816afbf0355423d650bc83c35e627e1b40fe
hy <wwdc14@vip.qq.com>
2020-06-10 09:29:19 +0800

    set platforms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "CommonExtensionsHelps",
+    platforms: [.iOS(.v10)],
     products: [
         .library(
             name: "CommonExtensionsHelps",
